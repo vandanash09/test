c30377eea3ed0db1d7b4134e9a64b6f99060aad4
Vandana Sharma <31274919+vandanash09@users.noreply.github.com>
2025-05-08 12:10:10 +0000

    Updated Trial.java

--- Trial.java
+++ Trial.java
@@ -1,5 +1,6 @@
 public class Trial {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!!!!");
+        System.out.println("Successfull Github Actions Implementation!!!");
     }
 }
